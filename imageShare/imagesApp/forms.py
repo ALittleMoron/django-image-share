a7a4f69ff846a798642dc4e9bbda4f94bb6812b6
ALittleMoron <dima.lunev14@gmail.com>
2021-07-11 20:41:41 +0300

    формы для проекта (перекачевали из предыдущего проекта, но я заменил некоторые названия)

---
+++ imageShare/imagesApp/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from .models import ImageWithContent
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(
+        label='Имя пользователя',
+        widget=forms.TextInput(attrs={"class": 'form-control'}))
+    password = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={"class": 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ()
+
+
+class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        label='Имя пользователя',
+        help_text="Обязательное поле. Не более 150 символов. Только буквы, цифры и символы @/./+/-/_.",
+        widget=forms.TextInput(attrs={"class": 'form-control'}))
+    email = forms.EmailField(
+        label='E-mail',
+        widget=forms.EmailInput(attrs={"class": 'form-control'}))
+    password1 = forms.CharField(
+        label='Пароль',
+        help_text="Пароль должен быть нераспространенным, не состоять только из цифр, состоять более чем из 8 символов.",
+        widget=forms.PasswordInput(attrs={"class": 'form-control'}))
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        help_text="Пароли должны совпадать.",
+        widget=forms.PasswordInput(attrs={"class": 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        
+        
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = ImageWithContent
+        fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(attrs={"class": 'form-control'}),
+            'content': forms.Textarea(attrs={"class": 'form-control'}),
+            'category': forms.Select(attrs={"class": 'form-control'}),
+        }
